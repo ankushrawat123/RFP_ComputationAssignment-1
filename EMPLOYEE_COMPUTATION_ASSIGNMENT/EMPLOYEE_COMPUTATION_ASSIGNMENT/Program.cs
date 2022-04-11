a9fecbb292c1ec7543be8838349af01d95644dad
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-11 23:59:45 +0530

    uploading uc_2 wage file

--- EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Program.cs
+++ EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EMPLOYEE_COMPUTATION_ASSIGNMENT;
+
 Console.WriteLine("Hello, World!");
 Uc1_Attendance.Attendance();
+Uc2_Wage.Wage();
--- EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Uc2_Wage.cs
+++ EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Uc2_Wage.cs
@@ -6,22 +6,29 @@ using System.Threading.Tasks;
 
 namespace EMPLOYEE_COMPUTATION_ASSIGNMENT
 {
-    internal class Uc1_Attendance
+    internal class Uc2_Wage
     {
-        public static void Attendance()
+        public static void Wage()
         {
             int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
             Random obj = new Random();
             int num = obj.Next(0, 2);
             if (num == Is_Full_Time)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
-            
+
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Employee wage : " + empWage);
         }
     }
+    
 }
