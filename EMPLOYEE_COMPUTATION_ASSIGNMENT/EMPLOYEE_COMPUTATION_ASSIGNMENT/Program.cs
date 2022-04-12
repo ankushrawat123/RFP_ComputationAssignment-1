c52015b11f3886ecf30ab2cb5a2aca167db0c6e4
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-12 09:35:26 +0530

    uploading Empwage in Uc4

---
+++ EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EMPLOYEE_COMPUTATION_ASSIGNMENT;
+Console.WriteLine("Hello, World!");
+Uc1_Attendance.Attendance();
+Uc2_Wage.Wage();
+Uc3_Parttime.Wage();
+Uc4_SwitchEmpWage.EmpWage();
