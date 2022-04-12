2263f4246b345b5c576385ac3d832b34064a4939
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-12 08:36:24 +0530

    uploading wage file in Uc3_Wage branch

---
+++ EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Uc1_Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPLOYEE_COMPUTATION_ASSIGNMENT
+{
+    internal class Uc1_Attendance
+    {
+        public static void Attendance()
+        {
+            int Is_Full_Time = 1;
+            Random obj = new Random();
+            int num = obj.Next(0, 2);
+            if (num == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
