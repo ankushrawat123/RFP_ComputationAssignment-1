25d695f2ca8f6a44a97305c86afd8e9c0f2f449b
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-12 16:40:21 +0530

    uploading uc9 file

--- EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Program.cs
+++ EMPLOYEE_COMPUTATION_ASSIGNMENT/EMPLOYEE_COMPUTATION_ASSIGNMENT/Program.cs
@@ -9,3 +9,12 @@ Uc5_20WorkingDays.Emp20DaysWage();
 Uc6_Max100hours.EmpMax100HrsWage();
 Uc7_Using_Wage_Method.method();
 Uc8_Wage_To_Multiple_Company.wagemethod();
+
+Console.WriteLine();
+Console.WriteLine("Uc9 Begin here ***********************************************");
+Uc9_Emp_Wage_Using_obj dMart = new Uc9_Emp_Wage_Using_obj("DMart",20,2,10);
+Uc9_Emp_Wage_Using_obj relience = new Uc9_Emp_Wage_Using_obj("Relience", 10, 4, 20);
+dMart.computeEmpWage();
+Console.WriteLine(dMart.toString());
+relience.computeEmpWage();
+Console.WriteLine(relience.toString());
